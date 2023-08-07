bc1f84b5483181e1519594e4359be885fa04d12b
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-08-07 11:12:22 -0300

    siguiendo

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -5,7 +5,7 @@ import { isLogged, isUser } from '../middleware/auth.js';
 const routerTicket = Router();
 
 routerTicket.get("/checkout", isLogged, isUser, ticketsController.checkOut);
-routerTicket.post("/checkout", isLogged, isUser, ticketsController.checkOut);
+routerTicket.post("/checkout", isLogged, isUser, ticketsController.addTicket); 
 routerTicket.get("/ticketfinished", isLogged, isUser, ticketsController.addTicket);
 
 export default routerTicket;
